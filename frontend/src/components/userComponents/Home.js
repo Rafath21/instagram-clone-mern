bd1308c19e1084099198efc239eb253cbdc1da08
Rafath21 <rafathmirza05@gmail.com>
2021-11-21 01:50:45 +0530

    Updated profile.js

--- frontend/src/components/userComponents/Home.js
+++ frontend/src/components/userComponents/Home.js
@@ -247,11 +247,9 @@ useEffect(()=>{
               ) : (
                 ""
               )}
-              <Link id="link" to={{ pathname: "/#header" }}>
                 <i class="fas fa-home" title="Home" id="home-icon" onClick={()=>{
                   window.scrollTo(0, 0)
                 }}></i>
-              </Link>
               <Link
                 className="link"
                 to={{
